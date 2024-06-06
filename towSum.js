3ae4820128b2c6056fd97746d020e774b786c68b
vishal <vkcodeblock@gmail.com>
2024-06-06 22:34:03 +0530

    first commit

---
+++ towSum.js
@@ -0,0 +1,23 @@
+let nums=[3,2,4,1,4,7,2,9,4,4];
+let target=6;
+
+var twoSum = function(nums, target) {
+    // const hashmap = new Map(); 
+    // for (let i = 0; i < nums.length; i++) {
+    //     console.log(hashmap)
+    //     const complement = target - nums[i]; 
+    //     if (hashmap.has(complement)) {
+           
+    //         return [hashmap.get(complement), i];
+    //     }
+    //     hashmap.set(nums[i], i);
+    // }
+    
+    // return null;
+ let j=nums.length-1;
+ for(let i=0;i<nums.length;i++){
+    
+ }
+
+}
+console.log(twoSum(nums,target))
