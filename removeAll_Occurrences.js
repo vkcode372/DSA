2fd0dc0a9e10474a9c93bf0b238858aafc94957e
vishal <vkcodeblock@gmail.com>
2024-10-06 21:12:47 +0530

    fixed the code in remove all occurrences

---
+++ removeAll_Occurrences.js
@@ -0,0 +1,22 @@
+/**
+ * Removes all occurrences of a specific value from an array and returns a new array.
+ *
+ * @param {Array} nums - The input array.
+ * @param {number} val - The value to be removed from the array.
+ * @returns {Array} A new array with all occurrences of the specified value removed.
+ */
+let nums = [0, 1, 2, 2, 3, 0, 4, 2];
+let val = 0;
+
+var removeElement = function (nums, val) {
+  let j = 0;
+  let newArr=[];
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      newArr[j] = nums[i];
+      j++;
+    }
+  }
+  return newArr;
+};
+console.log(removeElement(nums, val));
--- towSum.js
+++ towSum.js
@@ -1,23 +1,29 @@
+/**
+ * This function finds the indices of two numbers in an array that add up to a given target.
+ * It uses a hashmap to store the numbers and their indices as it iterates through the array.
+ * If a complement (target - current number) is found in the hashmap, the function returns the indices of the complement and the current number.
+ * If no such pair is found, the function returns null.
+ *
+ * @param {number[]} nums - The array of numbers to search.
+ * @param {number} target - The target sum to find.
+ * @returns {(number[]|null)} - An array containing the indices of the two numbers that add up to the target, or null if no such pair is found.
+ */
 let nums=[3,2,4,1,4,7,2,9,4,4];
-let target=6;
+let target=5;
 
 var twoSum = function(nums, target) {
-    // const hashmap = new Map(); 
-    // for (let i = 0; i < nums.length; i++) {
-    //     console.log(hashmap)
-    //     const complement = target - nums[i]; 
-    //     if (hashmap.has(complement)) {
-           
-    //         return [hashmap.get(complement), i];
-    //     }
-    //     hashmap.set(nums[i], i);
-    // }
-    
-    // return null;
- let j=nums.length-1;
- for(let i=0;i<nums.length;i++){
-    
- }
+    const hashmap = new Map(); 
+    for (let i = 0; i < nums.length; i++) {
+        console.log(hashmap)
+        const complement = target - nums[i]; 
+        if (hashmap.has(complement)) {
+
+            return [hashmap.get(complement), i];
+        }
+        hashmap.set(nums[i], i);
+    }
+
+    return null;
 
 }
 console.log(twoSum(nums,target))
