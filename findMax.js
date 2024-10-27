329a6d8bf9ed850a76540dc627fb3432fa71120e
vishal <vkcodeblock@gmail.com>
2024-10-27 23:09:26 +0530

    practice max number

---
+++ findMax.js
@@ -0,0 +1,32 @@
+/**
+ * This function returns the maximum of two numbers.
+ *
+ * @param {number} n1 - The first number.
+ * @param {number} n2 - The second number.
+ * @returns {number} The maximum of the two input numbers.
+ */
+function maxNumber(n1, n2) {
+  if (n1 > n2) return n1;
+  else return n2;
+}
+
+/**
+ * This function finds the maximum value in an array using recursion.
+ * If the array is empty, it returns null.
+ *
+ * @param {Array.<number>} arr - The array of numbers to find the maximum value from.
+ * @param {number} [startIndex=0] - The index to start searching from. Default is 0.
+ * @returns {number|null} The maximum value in the array or null if the array is empty.
+ */
+function findMax(arr, startIndex = 0) {
+  if (arr.length === 0) {
+    return null;
+  }
+  if (startIndex === arr.length - 1) {
+    return arr[startIndex];
+  }
+  return maxNumber(arr[startIndex], findMax(arr, startIndex + 1));
+}
+
+const ans = findMax([2, 4, 1, 1000, 45, 23, 100]);
+console.log(ans);
--- practic2.js
+++ practic2.js
@@ -1,19 +1,25 @@
-// count  the number of Occurrences for each string values 
-
-let string="leetcode";
-function count(string){
-     let myobj={};
-     if(string.length==0) return "provide the string";
-     for(let i=0;i<string.length;i++){
-         if(!myobj[string[i]]){
-             myobj[string[i]]=1;
-         }else{
-          myobj[string[i]]= myobj[string[i]]+1;; 
+/**
+ * Counts the number of occurrences for each string value in the given string.
+ *
+ * @param {string} string - The input string to count occurrences.
+ * @returns {Object} - An object where keys are unique characters from the input string,
+ * and values are the corresponding counts of each character.
+ * If the input string is empty, returns a message "provide the string".
+ *
+ * @example
+ * let string = "leetcode";
+ * let result = count(string);
+ * console.log(result); // Output: { l: 1, e: 2, t: 1, c: 1, o: 1, d: 1 }
+ */
+function count(string) {
+     let myobj = {};
+     if (string.length === 0) return "provide the string";
+     for (let i = 0; i < string.length; i++) {
+         if (!myobj[string[i]]) {
+             myobj[string[i]] = 1;
+         } else {
+             myobj[string[i]] = myobj[string[i]] + 1;
          }
-         
      }
-     return myobj
+     return myobj;
 }
-
-let result=count(string);
-console.log(result)
