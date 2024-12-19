6c3a4cb6b5df8819f5ace62e7f28e50f4cd588cb
vishal <vkcodeblock@gmail.com>
2024-12-19 21:42:08 +0530

    first character of each string in the given array to lowercase.

--- Easy/setArrayFirstElementCharaterToLowerCase.js
+++ Easy/setArrayFirstElementCharaterToLowerCase.js
@@ -6,6 +6,7 @@ let arr=["Cat","Window","Power"];
  * @param {string[]} arr - The array of strings to be processed.
  * @returns {string[]} The modified array with the first character of each string in lowercase.
  */
+
 function changeToLowerCase(arr) {
     let n = arr.length;
     for (let i = 0; i < n; i++) {
