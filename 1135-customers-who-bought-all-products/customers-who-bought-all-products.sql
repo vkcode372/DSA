035721c197c6689c9779f02f570507408be3e894
Vishal vishwakarma <105082445+vkcode372@users.noreply.github.com>
2024-12-19 22:26:19 +0530

    Time: 1329 ms (5.38%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT c.customer_id
+FROM Customer c
+INNER JOIN Product p
+ON c.product_key = p.product_key
+GROUP BY c.customer_id
+HAVING COUNT(DISTINCT c.product_key)=(SELECT COUNT(DISTINCT product_key) FROM Product)
+
