bfcd7722323288a4b84af408a8e1040ee335eed6
Vishal vishwakarma <105082445+vkcode372@users.noreply.github.com>
2024-12-20 23:22:22 +0530

    Time: 1 ms (76.91%) | Memory: 50.2 MB (94.28%) - LeetSync

---
+++ 136-single-number/single-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let answer=0;
+    for(let i=0;i<=nums.length;i++){
+       answer^=nums[i];
+    }
+    return answer;
+};
