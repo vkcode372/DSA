4b33de4227547989fcfed1efae500bee462cd343
Vishal vishwakarma <105082445+vkcode372@users.noreply.github.com>
2024-12-20 23:47:22 +0530

    Time: 18 ms (21.05%) | Memory: 55.8 MB (8.95%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -3,9 +3,22 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    let answer=0;
-    for(let i=0;i<=nums.length;i++){
-       answer^=nums[i];
+    let obj={};
+   for(let i=0;i<nums.length;i++){
+       if(!obj.hasOwnProperty(nums[i])){
+        obj[nums[i]]=false
+       }else{
+        obj[nums[i]]=true;
+       }
+   }
+    for(let [key,value] of Object.entries(obj)){
+      if(value==false){
+        return Number(key)
+      }
     }
-    return answer;
+    // let answer=0;
+    // for(let i=0;i<=nums.length;i++){
+    //    answer^=nums[i];
+    // }
+    // return answer;
 };
